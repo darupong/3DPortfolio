064148bdcdbd8f4653b71b6edb92a7a5e2257156
darupong <darupong007@gmail.com>
2023-07-12 21:36:33 +0700

    fix: optimize

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <Navbar />
         <div className="">
           <HeroSection />
-          <StarsCanvas />
+          {/* <StarsCanvas /> */}
         </div>
         <Footer />
       </div>
--- src/components/canvas/Keyboard.jsx
+++ src/components/canvas/Keyboard.jsx
@@ -14,8 +14,8 @@ const Keyboard = ({ isMobile }) => {
       <hemisphereLight intensity={0.15} groundColor="black" />
       <spotLight
         position={[20, 50, 10]}
-        angle={1}
-        penumbra={12}
+        angle={2}
+        penumbra={10}
         intensity={1}
         castShadow
         shadow-mapSize={10}
